908b3911bbb6764eea3c4fed19f3a8b7661b29aa
Chris Fotso <chrisfotso@gmail.com>
2019-02-22 20:20:51 -0600

    Added very basic functionality, ability to show book titles; no styles yet

---
+++ script.js
@@ -0,0 +1,43 @@
+const searchBox = document.getElementById('searchBox');
+const searchButton = document.getElementById('searchButton');
+const mainDiv = document.getElementById('main');
+const API_KEY = 'AIzaSyCHzFyndY3mRU0WOqvBwTxnCCrwL1wam54';
+
+searchButton.addEventListener('click', handleSearch);
+
+(() => {
+    searchBox.value = '';
+})();
+
+async function handleSearch() {
+    let inputValue = searchBox.value;
+    if (inputValue === '') {
+        alert('Please enter a search term.');
+        return;
+    } else {
+       let data = await getData(inputValue, API_KEY);
+       handleAndDisplayData(data);
+    }
+}
+
+async function getData(query, APIKey) {
+    let response = await fetch(`https:www.googleapis.com/books/v1/volumes?q=${query}?key=${APIKey}`);
+    let data = await response.json();
+    return data;
+}
+
+function handleAndDisplayData(data) {
+    let books = data.items;
+    if (!books) {
+        mainDiv.innerHTML = 'Sorry, no books found!';
+        return;
+    }
+    mainDiv.innerHTML = '';
+    let booksUl = document.createElement('ul');
+    mainDiv.appendChild(booksUl);
+    books.forEach(book => {
+        let bookLi = document.createElement('li');
+        bookLi.textContent = book.volumeInfo.title;
+        booksUl.appendChild(bookLi);
+    })
+}
